d0004ae2625ce6cf77d43d680a1d3ef7b5613f03
kieronjmckenna <kieron123.mckenna@gmail.com>
2023-09-19 11:04:24 +0100

    init

--- lib/cloudfront-function-medium-stack.ts
+++ lib/cloudfront-function-medium-stack.ts
@@ -1,16 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import { Construct } from "constructs";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+
+const DOMAIN = "yourdomain.com";
 
 export class CloudfrontFunctionMediumStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const responseFunction = new cloudfront.Function(
+      this,
+      "AppStoreFunctions",
+      {
+        code: cloudfront.FunctionCode.fromFile({
+          filePath: "src/functions/app-store-redirect.js",
+        }),
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudfrontFunctionMediumQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const certificate = new acm.Certificate(
+      this,
+      "AppStoreRedirectCertificate",
+      {
+        domainName: DOMAIN,
+        validation: acm.CertificateValidation.fromDns(
+          route53.HostedZone.fromHostedZoneId(
+            this,
+            "AppStoreRedirectHostedZoneFromId",
+            "<Your Hosted Zone Arn>"
+          )
+        ),
+      }
+    );
+
+    const bucket = new s3.Bucket(this, "AppStoreRedirectCloudfrontBucket");
+
+    new cloudfront.Distribution(this, "AppStoreRedirectDistribution", {
+      domainNames: [DOMAIN],
+      certificate,
+      // disable logging if not debugging, if enabled this will create a bucket for you
+      enableLogging: true,
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(bucket),
+        originRequestPolicy:
+          cloudfront.OriginRequestPolicy.ALL_VIEWER_AND_CLOUDFRONT_2022,
+        functionAssociations: [
+          {
+            eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+            function: responseFunction,
+          },
+        ],
+      },
+    });
   }
 }
